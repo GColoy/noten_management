6d069a629351e1e20acb22083acbc2f0094da179
Felix <felix.camrath@gmail.com>
2025-10-03 18:59:48 +0200

    changed styling to vueify style classes

--- frontend/src/views/index.vue
+++ frontend/src/views/index.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="home">
-    <div class="wrapper">
-      <div class="title">
+  <div class="d-flex flex-column align-center ma-3">
+    <div class="d-flex flex-column align-stretch justify-start w-100" style="max-width: 900px;">
+      <div class="text-h3 pb-12 text-center" style="font-family: 'DidactGothic', sans-serif;">
         <p>Noten Verwaltung</p>
       </div>
-      <Searchbar />
+      <Searchbar class="mb-8"/>
       <Dashboard />
+
     </div>
   </div>
 </template>
@@ -14,29 +15,3 @@
 import Searchbar from '../components/Searchbar.vue'
 import Dashboard from '../components/Dashboard.vue';
 </script>
-
-<style scoped>
-.home {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin: 12px;
-}
-.wrapper {
-  display: flex;
-  flex-direction: column;
-  align-items: stretch;
-  justify-content: flex-start;
-  width: 100%;
-  max-width: 50rem;
-  height: max-content;
-  /* padding: 1rem; */
-}
-.title {
-  font-size: 2rem;
-  font-weight: bold;
-  margin-bottom: 1rem;
-  font-family: 'DidactGothic', sans-serif;
-  text-align: center;
-}
-</style>
